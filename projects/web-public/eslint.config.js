194068b5bef57a42ffcbb57756314e920be3c316
Vinicius Ricardo Rosa Noli <viniciusnoli2016@gmail.com>
2024-05-24 14:47:10 -0300

    feat: added eslint to project configuration

---
+++ projects/web-public/eslint.config.js
@@ -0,0 +1,1 @@
+module.exports = require('@architecture/eslint-commons/base')
---
+++ setup/eslint-commons/base.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "extends": [
+    "next",
+    "eslint:recommended",
+    "plugin:prettier/recommended"
+  ]
+}
